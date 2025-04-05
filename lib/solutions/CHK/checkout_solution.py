3183a6ea26202aa11402ba9fad4aa1261241ab96
sbx_user1051 <sbx_user1051@169.254.56.81>
2025-04-05 20:22:24 +0000

    Sat Apr 05 20:22:24 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,13 +1,16 @@
-
 class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
         if not isinstance(skus, str):
             return -1
-            
+
+        items = {"A": 0, "B": 0, "C": 0, "D": 0}
+
         for char in skus:
-            if char not in 'ABCD':
+            if char not in items:
                 return -1
-                
+            items[char] += 1
+
         return None
+
