e28d33cd4433d25776eb0e8e97dee3e47bcc82ef
sbx_user1051 <sbx_user1051@169.254.56.81>
2025-04-05 20:02:28 +0000

    Sat Apr 05 20:02:28 UTC 2025

--- test/solution_tests/HLO/test_hlo_solution.py
+++ test/solution_tests/HLO/test_hlo_solution.py
@@ -7,3 +7,10 @@ class TestSum(TestCase):
         """Test that the compute method returns the sum of two numbers"""
         assert HelloSolution().hello("John") == "Hello, John!"
 
+    def test_hello_craftsman(self):
+        assert HelloSolution().hello("Craftsman") == "Hello, World!"
+
+    def test_hello_mr_x(self):
+        assert HelloSolution().hello("Mr. X") == "Hello, Mr. X!"
+
+
