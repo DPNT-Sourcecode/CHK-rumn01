082978f096e4e55ebf616f60344c09e83ca510f8
sbx_user1051 <sbx_user1051@169.254.56.81>
2025-04-05 20:45:07 +0000

    Sat Apr 05 20:45:07 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,22 +1,25 @@
 class CheckoutSolution:
 
-    ITEM_PRICES = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
     ITEM_TOTAL_CALCULATIONS = {
-        "A": lambda x: (x // 3) * 130 + (x % 3) * ITEM_PRICES['A'],
+        "A": lambda x: min(
+            (x // 3) * 130 + (x % 3) * 50,
+            x * 50,
+            (x // 5) * 200 + (x % 5) * 50,
+            x * 50,
+        ),
         "B": lambda x: (x // 2) * 45 + (x % 2) * 30,
         "C": lambda x: x * 20,
         "D": lambda x: x * 15,
-        "E": lambda x: x * 40
+        "E": lambda x: x * 40,
     }
 
     # skus = unicode string
     def checkout(self, skus):
 
-
         if not isinstance(skus, str):
             return -1
 
-        items = {"A": 0, "B": 0, "C": 0, "D": 0}
+        items = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0}
 
         for char in skus:
             if char not in items:
@@ -26,6 +29,7 @@ class CheckoutSolution:
         total = 0
         for item, count in items.items():
             total += self.ITEM_TOTAL_CALCULATIONS[item](count)
-            
-        return total 
+
+        return total
+
 
