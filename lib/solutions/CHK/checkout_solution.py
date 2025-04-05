bf47904ada1b87197fe286edc904ac416961ed03
sbx_user1051 <sbx_user1051@169.254.56.81>
2025-04-05 20:25:24 +0000

    Sat Apr 05 20:25:24 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,13 @@
 class CheckoutSolution:
 
+    ITEM_PRICES = {"A": 50, "B": 30, "C": 20, "D": 15}
+    ITEM_TOTAL_CALCULATIONS = {
+        "A": lambda x: (x // 3) * 130 + (x % 3) * 50,
+        "B": lambda x: (x // 2) * 45 + (x % 2) * 30,
+        "C": lambda x: x * 20,
+        "D": lambda x: x * 15
+    }
+
     # skus = unicode string
     def checkout(self, skus):
         if not isinstance(skus, str):
@@ -12,5 +20,8 @@ class CheckoutSolution:
                 return -1
             items[char] += 1
 
+        
+            
         return None
+
 
