b72a9cb37f7d69e8ee0dca41ed37217012f1a113
sbx_user1051 <sbx_user1051@169.254.56.81>
2025-04-05 20:13:24 +0000

    Sat Apr 05 20:13:24 UTC 2025

--- test/solution_tests/CHK/test_chk_solution.py
+++ test/solution_tests/CHK/test_chk_solution.py
@@ -5,3 +5,6 @@ class TestSum(TestCase):
     def test_sum(self):
         assert CheckoutSolution().checkout("ABCD") == 115
 
+
+    def test_invalid_sku_with_valid_skus(self): 
+        assert CheckoutSolution().checkout("ABCDAE") == -1
