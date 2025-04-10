88aea24cb1f714f45d5008a0987dbc0429373f18
sbx_user1051 <sbx_user1051@169.254.3.149>
2025-04-10 21:43:15 +0000

    Thu Apr 10 21:43:15 UTC 2025

--- test/solution_tests/CHK/test_chk_solution.py
+++ test/solution_tests/CHK/test_chk_solution.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
-from solutions.CHK.checkout_solution import CheckoutSolution
+from solutions.CHK.checkout_solution import CheckoutSolution   
+
 
 class TestSum(TestCase):
 
-    def test_invalid_sku_with_valid_skus(self): 
+    def test_invalid_sku_with_valid_skus(self):
         assert CheckoutSolution().checkout("ABCDAZ") == -1
 
     def test_non_unicode_input(self):
@@ -23,20 +24,22 @@ class TestSum(TestCase):
 
     def test_offer_3a_triggered(self):
         assert CheckoutSolution().checkout("AAABCDE") == 235
+
     def test_offer_5a_triggered(self):
         assert CheckoutSolution().checkout("AAAAAABCDE") == 355
+
     def test_offer_2b_triggered(self):
         assert CheckoutSolution().checkout("ABBCDE") == 170
 
     def test_offer_2e_triggered(self):
         assert CheckoutSolution().checkout("ABBEE") == 120
+
     def test_multiple_offers_returns_lowest_total(self):
         # 5A could be calculated as (3A + 2A) = 130 + 100 = 230
         # Or as 5A = 200
         # Should return 200 as it's the lower value
         assert CheckoutSolution().checkout("AAAAA") == 200
 
-    
     def test_multiple_offers_favors_larger_groups(self):
         # 6A could be calculated as (3A + 3A) = 130 + 130 = 260
         # Or as (5A + 1A) = 200 + 50 = 250
@@ -47,5 +50,6 @@ class TestSum(TestCase):
         # 4E + 2B = (4 * 40) + (2 * 30) - (2 * 30) = 160 + 0 = 160
         # The 4E triggers 2 free B's, which cancels out the cost of the 2 B's
         assert CheckoutSolution().checkout("EEEEBB") == 160
+
 
 
