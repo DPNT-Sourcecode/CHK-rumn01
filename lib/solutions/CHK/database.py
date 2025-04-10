643006b521ed459d3a6bbf2d36050fc5e64e4695
sbx_user1051 <sbx_user1051@169.254.4.181>
2025-04-10 19:03:07 +0000

    Thu Apr 10 19:03:07 UTC 2025

--- lib/solutions/CHK/database.py
+++ lib/solutions/CHK/database.py
@@ -3,19 +3,33 @@
 from lib.solutions.CHK.models import Inventory
 
 mock_database = {
-    "items": {"sku": "A", "price": 50},
-    "offers": {"sku": "A", "multiplier": 3, "offer_value": 130},
-    "cross_offers": {
-        "primary_item_sku": "E",
-        "primary_item_multiplier": 2,
-        "offer_item_sku": "B",
-        "offer_item_multiplier": 1,
-        "offer_value": 0,
-    },
+    "items": [
+        {"sku": "A", "price": 50},
+        {"sku": "B", "price": 30},
+        {"sku": "C", "price": 20},
+        {"sku": "D", "price": 15},
+        {"sku": "E", "price": 40},
+    ],
+    "offers": [
+        {"sku": "A", "multiplier": 3, "offer_value": 130},
+        {"sku": "A", "multiplier": 5, "offer_value": 200},
+        {"sku": "B", "multiplier": 2, "offer_value": 45},
+    ],
+    "cross_offers": [
+        {
+            "primary_item_sku": "E",
+            "primary_item_multiplier": 2,
+            "offer_item_sku": "B",
+            "offer_item_multiplier": 1,
+            "offer_value": 0,
+        }
+    ],
 }
 
+def mock_get_items_query(skus:list[str])
 
 Inventory()
+
 
 
 
