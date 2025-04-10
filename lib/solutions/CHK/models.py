1d79a516d41ea0bec3d9ee2670900a7427967451
sbx_user1051 <sbx_user1051@169.254.42.87>
2025-04-10 18:15:45 +0000

    Thu Apr 10 18:15:45 UTC 2025

--- lib/solutions/CHK/models.py
+++ lib/solutions/CHK/models.py
@@ -3,23 +3,29 @@ from decimal import Decimal
 
 class Offer:
     """Class to describe an offer applied to a single item
-    
+
     This could take the form 3{item} for {total_price}
+
+    Args:
+        multiplier (int): Number of items to trigger an offer
+        offer_value (Decimal): Value of the offer when multiplier of items 
+            is reached
     """
-    def __init__(self, multiplier:int, value:Decimal):
-        self.multiplier = multiplier
-        self.value = value
+
+    def __init__(self, multiplier: int, offer_value: Decimal):
+        self.multiplier: int = multiplier
+        self.offer_value: Decimal = offer_value
 
 
 class CrossOffer:
-    def __init__(self, item: str, multiplier, reduction):
-        self.item = item
+    def __init__(self, item: str, multiplier:int, reduction):
+        self.item: str = item
         self.multiplier = multiplier
         self.reduction = reduction
 
 
 class Item:
-    
+
     def __init__(
         self,
         sku: str,
